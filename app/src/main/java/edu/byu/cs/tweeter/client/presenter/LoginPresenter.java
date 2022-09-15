187e6e1f5e985813684edafbfb80d1508225c6c9
optimisms <optimisms@users.noreply.github.com>
2022-09-14 22:38:49 -0600

    Implemented Presenter class as best as possible from memory & notes

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
@@ -18,17 +18,32 @@ public class LoginPresenter implements UserService.LoginObserver {
     public LoginPresenter(View inView) { mView = inView; }
 
     public void initiateLogin(String username, String password) {
-        //new UserService().login(username, password, this);  //Pretty sure this is wrong but idr how
+        new UserService().login(username, password, this);  //Pretty sure this is wrong but idr how
+        String message = validateLogin(username, password);
+        if (message != null) { mView.displayErrorMessage(message); }
+        else {
+            mView.clearErrorMessage();
+            mView.displayInfoMessage("Logging in...");
+        }
+    }
 
+    public String validateLogin(String username, String password) {
+        if (username.charAt(0) != '@') {return "Username must begin with @.";}
+        if (username.length() < 2) {return "Alias must contain 1 or more characters after the @.";}
+        if (password.length() == 0) {return "Password cannot be empty.";}
+        else return null;
     }
 
     @Override
     public void loginSuccess(User user, AuthToken authToken) {
-
+        mView.clearInfoMessage();
+        mView.clearErrorMessage();
+        mView.navigateToUser(user);
     }
 
     @Override
     public void loginFailed(String message) {
-
+        mView.clearInfoMessage();
+        mView.displayErrorMessage(message);
     }
 }
