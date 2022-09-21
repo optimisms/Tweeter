6675726547c5bd0cf4fa5195b945a6cac715878b
optimisms <optimisms@users.noreply.github.com>
2022-09-21 01:31:43 -0600

    Optimized imports

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/UserService.java
@@ -1,9 +1,7 @@
 package edu.byu.cs.tweeter.client.model.service;
 
-import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -13,8 +11,6 @@ import java.util.concurrent.Executors;
 import edu.byu.cs.tweeter.client.backgroundTask.LoginTask;
 import edu.byu.cs.tweeter.client.backgroundTask.RegisterTask;
 import edu.byu.cs.tweeter.client.cache.Cache;
-import edu.byu.cs.tweeter.client.view.login.RegisterFragment;
-import edu.byu.cs.tweeter.client.view.main.MainActivity;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
