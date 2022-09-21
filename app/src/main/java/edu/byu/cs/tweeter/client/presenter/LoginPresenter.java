bd446de3189263e7ea177b4c4ed9cf82183d011f
optimisms <optimisms@users.noreply.github.com>
2022-09-21 00:54:15 -0600

    Implemented LoginPresenter class in full, checked against class video

--- app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/LoginPresenter.java
@@ -18,20 +18,22 @@ public class LoginPresenter implements UserService.LoginObserver {
     public LoginPresenter(View inView) { mView = inView; }
 
     public void initiateLogin(String username, String password) {
-        new UserService().login(username, password, this);  //Pretty sure this is wrong but idr how
         String message = validateLogin(username, password);
-        if (message != null) { mView.displayErrorMessage(message); }
-        else {
+        if (message != null) { //Login invalid
+            mView.clearInfoMessage();
+            mView.displayErrorMessage(message); }
+        else { //Login valid
             mView.clearErrorMessage();
             mView.displayInfoMessage("Logging in...");
+            new UserService().login(username, password, this);
         }
     }
 
     public String validateLogin(String username, String password) {
-        if (username.charAt(0) != '@') {return "Username must begin with @.";}
-        if (username.length() < 2) {return "Alias must contain 1 or more characters after the @.";}
-        if (password.length() == 0) {return "Password cannot be empty.";}
-        else return null;
+        if (username.charAt(0) != '@') { return "Username must begin with @."; }
+        if (username.length() < 2) { return "Alias must contain 1 or more characters after the @."; }
+        if (password.length() == 0) { return "Password cannot be empty."; }
+        return null;
     }
 
     @Override
