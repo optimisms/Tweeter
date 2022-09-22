9a5d02bca67f9a2bcdc684016cb080f118959d1c
optimisms <optimisms@users.noreply.github.com>
2022-09-22 15:15:19 -0600

    Moved GetStoryTask to complete

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/StoryService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/StoryService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import edu.byu.cs.tweeter.client.backgroundTask.GetStoryTask;
+import edu.byu.cs.tweeter.client.backgroundTask.complete.GetStoryTask;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.Status;
 import edu.byu.cs.tweeter.model.domain.User;
