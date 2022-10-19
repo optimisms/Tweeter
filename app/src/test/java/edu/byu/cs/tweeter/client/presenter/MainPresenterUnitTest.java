f7ac97a0ebc615f05d7c2c5b47a2122a50864140
optimisms <optimisms@users.noreply.github.com>
2022-10-19 15:46:31 -0600

    Created Unit Test class

---
+++ app/src/test/java/edu/byu/cs/tweeter/client/presenter/MainPresenterUnitTest.java
@@ -0,0 +1,20 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import org.junit.jupiter.api.Test;
+
+public class MainPresenterUnitTest {
+    @Test
+    public void testPostStatusSuccess() {
+
+    }
+
+    @Test
+    public void testPostStatusFailure() {
+
+    }
+
+    @Test
+    public void testPostStatusException() {
+
+    }
+}
