4a3a5d4a18c78d9bc1ee03c5631ef14ad9be06ee
optimisms <optimisms@users.noreply.github.com>
2022-11-22 00:04:07 -0700

    Removed two more small inheritance errors

--- app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/GetFollowersTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/GetFollowersTask.java
@@ -3,14 +3,12 @@ package edu.byu.cs.tweeter.client.backgroundTask;
 import android.os.Handler;
 
 import java.io.IOException;
-import java.util.List;
 
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.model.domain.User;
 import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
 import edu.byu.cs.tweeter.model.net.request.GetFollowersRequest;
 import edu.byu.cs.tweeter.model.net.response.PagedResponse;
-import edu.byu.cs.tweeter.util.Pair;
 
 /**
  * Background task that retrieves a page of followers.
@@ -24,10 +22,5 @@ public class GetFollowersTask extends PagedUserTask {
         String lastFollowerAlias = getLastItem() == null ? null : getLastItem().getAlias();
         GetFollowersRequest req = new GetFollowersRequest(getAuthToken(), targetUserAlias, getLimit(), lastFollowerAlias);
         return getServerFacade().getFollowers(req, FollowService.GET_FOLLOWERS_URL_PATH);
-    }
-
-    @Override
-    protected Pair<List<User>, Boolean> getItems() {
-        return getFakeData().getPageOfUsers(getLastItem(), getLimit(), getTargetUser());
     }
 }
--- app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/GetFollowingTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/GetFollowingTask.java
@@ -3,14 +3,12 @@ package edu.byu.cs.tweeter.client.backgroundTask;
 import android.os.Handler;
 
 import java.io.IOException;
-import java.util.List;
 
 import edu.byu.cs.tweeter.client.model.service.FollowService;
 import edu.byu.cs.tweeter.model.domain.User;
 import edu.byu.cs.tweeter.model.net.TweeterRemoteException;
 import edu.byu.cs.tweeter.model.net.request.GetFollowingRequest;
 import edu.byu.cs.tweeter.model.net.response.PagedResponse;
-import edu.byu.cs.tweeter.util.Pair;
 
 /**
  * Background task that retrieves a page of other users being followed by a specified user.
@@ -24,10 +22,5 @@ public class GetFollowingTask extends PagedUserTask {
         String lastFolloweeAlias = getLastItem() == null ? null : getLastItem().getAlias();
         GetFollowingRequest req = new GetFollowingRequest(getAuthToken(), targetUserAlias, getLimit(), lastFolloweeAlias);
         return getServerFacade().getFollowees(req, FollowService.GET_FOLLOWING_URL_PATH);
-    }
-
-    @Override
-    protected Pair<List<User>, Boolean> getItems() {
-        return getFakeData().getPageOfUsers(getLastItem(), getLimit(), getTargetUser());
     }
 }
