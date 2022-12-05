4af968abec939f38795972c7ee364fed527a3b51
optimisms <optimisms@users.noreply.github.com>
2022-12-05 16:27:00 -0700

    Function to verify authToken expiration

---
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/AuthService.java
@@ -0,0 +1,28 @@
+package edu.byu.cs.tweeter.server.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+
+public class AuthService {
+    public boolean tokenIsValid(AuthToken token) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:m");
+
+        Date authDate = sdf.parse(token.getDatetime());
+        Date now = Calendar.getInstance().getTime();
+
+//        System.out.println("authDate: " + authDate.toString());
+//        System.out.println("now: " + now);
+
+        long diffInMillies = Math.abs(authDate.getTime() - now.getTime());
+        long diff = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
+
+        System.out.println("time since auth token: " + diff);
+
+        return diff <= 30;
+    }
+}
