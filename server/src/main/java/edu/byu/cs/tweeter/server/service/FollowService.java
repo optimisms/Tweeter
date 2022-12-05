0a3f877432c0847a52921d0fdc6cd110c04df665
optimisms <optimisms@users.noreply.github.com>
2022-12-05 13:30:00 -0700

    Fixed error handling (hopefully) for implemented methods so far

--- server/src/main/java/edu/byu/cs/tweeter/server/service/FollowService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/FollowService.java
@@ -25,38 +25,38 @@ import edu.byu.cs.tweeter.server.dao.factory.DynamoDAOFactory;
  * Contains the business logic for getting the users a user is following.
  */
 public class FollowService {
-    //TODO: Check right error handling for all methods
-
+    //TODO: add AuthToken checking to each method
     public IsFollowerResponse isFollower(IsFollowerRequest request) {
-        if (request.getFollower() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a follower");
-        } else if (request.getFollowee() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a followee");
-        }
-
         try {
+            if (request.getFollower() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a follower");
+            } else if (request.getFollowee() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a followee");
+            }
             Follow relationship = getFollowDAO().get(request.getFollower().getAlias(), request.getFollowee().getAlias());
 
             if (relationship != null) { return new IsFollowerResponse(true); }
             else { return new IsFollowerResponse(false); }
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
             if (e.getMessage().startsWith("Item not found at PartitionKey (")) {
                 return new IsFollowerResponse(false);
+            } else if (e.getMessage().startsWith("[BadRequest]")) {
+                return new IsFollowerResponse(e.getMessage());
             } else {
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new IsFollowerResponse("[Internal Server Error] " + e.getMessage());
             }
         }
     }
 
     public FollowResponse follow(FollowRequest request) {
-        if (request.getFollower() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a follower");
-        } else if (request.getFollowee() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a followee");
-        }
-
         try {
+            if (request.getFollower() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a follower");
+            } else if (request.getFollowee() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a followee");
+            }
+
             Follow toAdd = new Follow(request.getFollower(), request.getFollowee());
 
             getFollowDAO().add(toAdd);
@@ -70,24 +70,25 @@ public class FollowService {
             getUserDAO().update(followee);
 
             return new FollowResponse();
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
-            if (e.getMessage().startsWith("User " + request.getFollower().getAlias())) {
+
+            if (e.getMessage().startsWith("[BadRequest]")) {
                 return new FollowResponse(e.getMessage());
             } else {
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new FollowResponse("[Internal Server Error] " + e.getMessage());
             }
         }
     }
 
     public UnfollowResponse unfollow(UnfollowRequest request) {
-        if (request.getUnfollower() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a unfollower");
-        } else if (request.getUnfollowee() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a unfollowee");
-        }
-
         try {
+            if (request.getUnfollower() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a unfollower");
+            } else if (request.getUnfollowee() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a unfollowee");
+            }
+
             Follow toDelete = new Follow(request.getUnfollower(), request.getUnfollowee());
 
             getFollowDAO().delete(toDelete);
@@ -101,86 +102,101 @@ public class FollowService {
             getUserDAO().update(unfollowee);
 
             return new UnfollowResponse();
-        } catch (DataAccessException e) {
-            if (e.getMessage().startsWith("User " + request.getUnfollower().getAlias())) {
+        } catch (DataAccessException | RuntimeException e) {
+            e.printStackTrace();
+
+            if (e.getMessage().startsWith("[BadRequest]")) {
                 return new UnfollowResponse(e.getMessage());
             } else {
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new UnfollowResponse("[Internal Server Error] " + e.getMessage());
             }
         }
     }
 
     public GetFollowingResponse getFollowees(PagedRequest<User> request) {
-        if(request.getTargetUserAlias() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a follower alias");
-        } else if(request.getLimit() <= 0) {
-            throw new RuntimeException("[BadRequest] Request needs to have a positive limit");
-        }
-
         try {
+            if(request.getTargetUserAlias() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a follower alias");
+            } else if(request.getLimit() <= 0) {
+                throw new RuntimeException("[BadRequest] Request needs to have a positive limit");
+            }
+
             String lastFolloweeAlias = request.getLastItem() == null ? null : request.getLastItem().getAlias();
             List<User> following = getFollowDAO().getFollowing(request.getTargetUserAlias(), request.getLimit(), lastFolloweeAlias);
 
             return new GetFollowingResponse(following, following.size() == request.getLimit());
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
-            throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+
+            if (e.getMessage().startsWith("[BadRequest]")) {
+                return new GetFollowingResponse(e.getMessage());
+            } else {
+                return new GetFollowingResponse("[Internal Server Error] " + e.getMessage());
+            }
         }
     }
 
     public GetFollowersResponse getFollowers(PagedRequest<User> request) {
-        if(request.getTargetUserAlias() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a followee alias");
-        } else if(request.getLimit() <= 0) {
-            throw new RuntimeException("[BadRequest] Request needs to have a positive limit");
-        }
-
         try {
+            if(request.getTargetUserAlias() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a followee alias");
+            } else if(request.getLimit() <= 0) {
+                throw new RuntimeException("[BadRequest] Request needs to have a positive limit");
+            }
+
             String lastFollowerAlias = request.getLastItem() == null ? null : request.getLastItem().getAlias();
             List<User> followers = getFollowDAO().getFollowers(request.getTargetUserAlias(), request.getLimit(), lastFollowerAlias);
 
             return new GetFollowersResponse(followers, followers.size() == request.getLimit());
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
-            throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+            if (e.getMessage().startsWith("[BadRequest]")) {
+                return new GetFollowersResponse(e.getMessage());
+            } else {
+                return new GetFollowersResponse("[Internal Server Error] " + e.getMessage());
+            }
         }
+
     }
 
     public CountResponse getFollowersCount(CountRequest request) {
-        if (request.getTargetUser() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a target user");
-        }
-
         try {
+            if (request.getTargetUser() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a target user");
+            }
+
             User user = getUserDAO().get(request.getTargetUser().getAlias(), null);
 
             return new CountResponse(user.getFollowerCount());
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
-            if (e.getMessage().startsWith("Item not found at PartitionKey (")) {
+
+            if (e.getMessage().startsWith("[BadRequest]")) {
                 return new CountResponse(e.getMessage());
             } else {
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new CountResponse("[Internal Server Error] " + e.getMessage());
             }
         }
     }
 
     public CountResponse getFollowingCount(CountRequest request) {
-        if (request.getTargetUser() == null) {
-            throw new RuntimeException("[BadRequest] Request needs to have a target user");
-        }
-
         try {
+            if (request.getTargetUser() == null) {
+                throw new RuntimeException("[BadRequest] Request needs to have a target user");
+            }
+
             User user = getUserDAO().get(request.getTargetUser().getAlias(), null);
 
             return new CountResponse(user.getFollowingCount());
         } catch (DataAccessException e) {
             e.printStackTrace();
-            if (e.getMessage().startsWith("Item not found at PartitionKey (")) {
+
+            if (e.getMessage().startsWith("[BadRequest]")) {
                 return new CountResponse(e.getMessage());
             } else {
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new CountResponse("[Internal Server Error] " + e.getMessage());
             }
+
         }
     }
 
--- server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/service/UserService.java
@@ -22,42 +22,40 @@ import edu.byu.cs.tweeter.server.dao.factory.DynamoDAOFactory;
 import edu.byu.cs.tweeter.util.FakeData;
 
 public class UserService {
-    //TODO: Check right error handling for all methods
-
+    //TODO: add checking for authToken validity
     public GetUserResponse getUser(GetUserRequest request) {
-        if (request.getAlias() == null) {
-            throw new RuntimeException("[BadRequest] Missing the user alias");
-        }
-
         try {
+            if (request.getAlias() == null) {
+                throw new RuntimeException("[BadRequest] Missing the user alias");
+            }
+
             User user = getNewUserDAO().get(request.getAlias(), null);
 
             return new GetUserResponse(user);
-        } catch (DataAccessException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
-            if (e.getMessage().startsWith("Item not found at PartitionKey (")) {
+            if (e.getMessage().startsWith("[BadRequest")) {
                 return new GetUserResponse(e.getMessage());
             } else {
-
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new GetUserResponse("[Internal Server Error] " + e.getMessage());
             }
         }
     }
 
     public AuthResponse register(RegisterRequest request) {
-        if(request.getUsername() == null){
-            throw new RuntimeException("[BadRequest] Missing a username");
-        } else if(request.getPassword() == null) {
-            throw new RuntimeException("[BadRequest] Missing a password");
-        } else if(request.getFirstName() == null) {
-            throw new RuntimeException("[BadRequest] Missing a first name");
-        } else if(request.getLastName() == null) {
-            throw new RuntimeException("[BadRequest] Missing a last name");
-        } else if(request.getImage() == null) {
-            throw new RuntimeException("[BadRequest] Missing an image");
-        }
-
         try {
+            if(request.getUsername() == null){
+                throw new RuntimeException("[BadRequest] Missing a username");
+            } else if(request.getPassword() == null) {
+                throw new RuntimeException("[BadRequest] Missing a password");
+            } else if(request.getFirstName() == null) {
+                throw new RuntimeException("[BadRequest] Missing a first name");
+            } else if(request.getLastName() == null) {
+                throw new RuntimeException("[BadRequest] Missing a last name");
+            } else if(request.getImage() == null) {
+                throw new RuntimeException("[BadRequest] Missing an image");
+            }
+
             String imageURL = S3.putImage(request.getUsername().substring(1), request.getImage());
 
             //Consider passing salt by reference so it only has to return one object? Can you even do that in Java?
@@ -66,27 +64,22 @@ public class UserService {
             User toAdd = new User(request.getFirstName(), request.getLastName(), request.getUsername(), imageURL, hashResults[0], hashResults[1]);
 
             getNewUserDAO().add(toAdd);
-//            getNewUserDAO().add(new User("d", "a", "@issddf", "asdfasdf", 0, 0));
 
             AuthToken token = generateNewAuthToken();
             getNewAuthTokenDAO().add(token);
 
             return new AuthResponse(toAdd, token);
-        //TODO: talk to a TA about what on earth is happening here to make it time out?????
-            //return new AuthResponse(new User("", "", "", ""), new AuthToken());
-        } catch (DataAccessException | NoSuchAlgorithmException e) {
+        } catch (DataAccessException | RuntimeException e) {
             e.printStackTrace();
-            if (e.getMessage().startsWith("The username") || e.getMessage().equals("This authToken already exists.")) {
+            if (e.getMessage().startsWith("[BadRequest")) {
                 return new AuthResponse(e.getMessage());
-            } else if (e.getClass() == NoSuchAlgorithmException.class) {
-                throw new RuntimeException("[Internal Server Error] Password hashing failed. Please try again later.");
             } else {
-                throw new RuntimeException("[Internal Server Error] " + e.getMessage());
+                return new AuthResponse("[Internal Server Error] " + e.getMessage());
             }
         }
     }
 
-    private byte[][] hashPassword(String password, byte[] salt) throws NoSuchAlgorithmException {
+    private byte[][] hashPassword(String password, byte[] salt) throws RuntimeException {
         //If called by login, salt is retrieved from DB and passed in
         //Otherwise, salt should be generated for register
         if (salt == null) {
@@ -95,7 +88,13 @@ public class UserService {
             random.nextBytes(salt);
         }
 
-        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Password hashing failed. Please try again later.");
+        }
         md.update(salt);
 
         byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
@@ -113,6 +112,7 @@ public class UserService {
     }
 
     //TODO: migrate below to new Dynamo DAOs
+    //TODO: Check right error handling for all methods
 
     public AuthResponse login(LoginRequest request) {
         if(request.getUsername() == null){
@@ -122,10 +122,6 @@ public class UserService {
         }
 
         // TODO: Generates dummy data. Replace with a real implementation.
-//        List<User> users = getFakeData().getFakeUsers();
-//        for (User user : users) {
-//            getNewUserDAO().add(user);
-//        }
 
         User user = getDummyUser();
         AuthToken authToken = getDummyAuthToken();
