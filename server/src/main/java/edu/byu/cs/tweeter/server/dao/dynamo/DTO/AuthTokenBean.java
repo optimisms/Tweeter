318d78127780293fa53f1f47e3786bd8ae34a6ce
optimisms <optimisms@users.noreply.github.com>
2022-12-05 01:38:39 -0700

    Forgot Bean tags on AuthTokenBean

--- server/src/main/java/edu/byu/cs/tweeter/server/dao/dynamo/DTO/AuthTokenBean.java
+++ server/src/main/java/edu/byu/cs/tweeter/server/dao/dynamo/DTO/AuthTokenBean.java
@@ -1,9 +1,14 @@
 package edu.byu.cs.tweeter.server.dao.dynamo.DTO;
 
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
 public class AuthTokenBean {
     private String token;
     private String datetime;
 
+    @DynamoDbPartitionKey
     public String getToken() {
         return token;
     }
